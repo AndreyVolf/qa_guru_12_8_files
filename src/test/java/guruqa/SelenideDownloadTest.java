ba43b16ae820a3b8bf1a432d48d20da6d3dc1c7d
Andrey <vtometall@gmail.com>
2022-04-26 17:08:08 +0300

    add test download try

--- src/test/java/guruqa/SelenideDownloadTest.java
+++ src/test/java/guruqa/SelenideDownloadTest.java
@@ -1,12 +1,20 @@
 package guruqa;
 
+import com.codeborne.pdftest.PDF;
+import com.codeborne.pdftest.matchers.ContainsExactText;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.collections.ContainExactTextsCaseSensitive;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 import static com.codeborne.selenide.Selenide.$;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SelenideDownloadTest {
 
@@ -14,11 +22,27 @@ public class SelenideDownloadTest {
     void downloadTest() throws FileNotFoundException {
         Selenide.open("https://github.com/junit-team/junit5/blob/main/README.md");
         File textFile = $("#raw-url").download();
-        System.out.println();
-
+        try  (InputStream is = new FileInputStream(textFile)); {
+        byte[] fileContent = is.readAllBytes();
+        String strContent = new String(fileContent, StandardCharsets.UTF_8);
+        assertThat(strContent).contains("Junit 5");
+    }
 
 
     }
+
+    @Test
+    void pdfParsingTest()  {
+
+
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("PDF/junit-user-guide-5.8.2.pdf");
+       PDF pdf = new PDF (stream);
+        Assertions.assertEquals(166,pdf.numberOfPages);
+        assertThat(pdf, new ContainsExactText("123");
+
+    }
+
+
 
 
 
